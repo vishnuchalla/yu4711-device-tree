9778506c15375d4008ea17ea00d69b523b50cf43
vishnuchalla <vishnuchalla47@gmail.com>
2017-07-31 09:31:30 +0530

    yu yunique

---
+++ sensors/Android.mk
@@ -0,0 +1,46 @@
+ifeq ($(TARGET_DEVICE),jalebi)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    sensors.cpp \
+    SensorBase.cpp \
+    LightSensor.cpp \
+    ProximitySensor.cpp \
+    Accelerometer.cpp \
+    InputEventReader.cpp \
+    CalibrationManager.cpp \
+    NativeSensorManager.cpp \
+    VirtualSensor.cpp \
+    sensors_XML.cpp
+
+LOCAL_CFLAGS += -DLOG_TAG=\"Sensors\"
+
+LOCAL_C_INCLUDES := \
+    external/libxml2/include \
+    external/icu/icu4c/source/common
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libutils
+
+LOCAL_MODULE := sensors.msm8916
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := calmodule.cfg
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
+LOCAL_SRC_FILES := calmodule.cfg
+
+include $(BUILD_PREBUILT)
+
+endif
